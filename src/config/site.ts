a3e42b52ad80def196492867376ada4caeaa1896
eduardohpg1 <eduardohpg1@gmail.com>
2026-05-13 15:55:39 -0300

    feat: corrige ícone Kwai e adiciona Threads no footer

--- src/config/site.ts
+++ src/config/site.ts
@@ -10,6 +10,7 @@ export const siteConfig = {
   tiktok: "https://www.tiktok.com/@alexiafreitas10?_r=1&_t=ZS-96Kl2bTqFPw",
   facebook: "https://www.facebook.com/share/1Cj7EPLnJf/?mibextid=wwXIfr",
   kwai: "https://k.kwai.com/u/@Alexiaachadinhos/BICK0x3Q",
+  threads: "https://www.threads.com/@alexia_compreaqui2?igshid=NTc4MTIwNjQ2YQ==",
   email: "contato@alexiacompreaqui.com",
   profileImage: "/images/2.jpeg",
   heroImage: "/images/hero-bg.jpg",
